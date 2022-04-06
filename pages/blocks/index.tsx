1bdbd42e00a371fe6460bafc43ee9d9aff841877
Denis Potapov <denipotapo@yandex.ru>
2022-04-06 19:30:17 +0300

    add nextjs native env variables

--- pages/blocks/index.tsx
+++ pages/blocks/index.tsx
@@ -1,34 +1,35 @@
 import { BlocksTable } from "../../components";
 import React, { useState, useEffect } from "react";
 
-const Blocks = ({ env }: any) => {
+const Blocks = () => {
   const [allBlocks, setAllBlocks] = useState([]);
   useEffect(() => {
     getBlocks();
     console.log("Blocks Page", allBlocks);
+    console.log(process.env.NEXT_PUBLIC_API_URL);
   }, [setAllBlocks]);
 
   const getBlocks = async () => {
-    const res = await fetch(env.API_URL);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`);
     const data = await res.json();
     setAllBlocks(data);
   };
 
   return (
     <>
-      <BlocksTable data={allBlocks} wssUrl={env.WSS_URL} />
+      <BlocksTable data={allBlocks} wssUrl={process.env.NEXT_PUBLIC_WSS_URL} />
     </>
   );
 };
 
 export default Blocks;
 
-export async function getServerSideProps() {
-  const env = {
-    API_URL: `${process.env.API_URL}`,
-    WSS_URL: `${process.env.WSS_URL}`,
-  };
-  return {
-    props: { env },
-  };
-}
+// export async function getServerSideProps() {
+//   const env = {
+//     API_URL: `${process.env.API_URL}`,
+//     WSS_URL: `${process.env.WSS_URL}`,
+//   };
+//   return {
+//     props: { env },
+//   };
+// }
