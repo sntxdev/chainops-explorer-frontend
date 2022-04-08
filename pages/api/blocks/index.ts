196bd32ea6ea5b34deaf4208d95d770db54c91b9
Denis Potapov <denipotapo@yandex.ru>
2022-04-09 00:48:40 +0300

    fix public api url

--- pages/api/blocks/index.ts
+++ pages/api/blocks/index.ts
@@ -1,1 +1,23 @@
-export {};
+import type { NextApiRequest, NextApiResponse } from "next";
+import { conn as pool } from "../../../utils/database";
+
+type Data = {
+  name: string;
+};
+
+async function retrieveData() {}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM block ORDER BY height DESC LIMIT 20 "
+    );
+    console.log(result.rows);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+  }
+}
