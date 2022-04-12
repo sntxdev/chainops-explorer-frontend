fa58c7c482e6f437a8239396f7b30bc9f7890aef
Denis Potapov <denipotapo@yandex.ru>
2022-04-12 18:26:17 +0300

    fix fix cors

--- next.config.js
+++ next.config.js
@@ -17,26 +17,26 @@ module.exports = {
   },
 };
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: 'https://explorer.chainops.org/api/archway/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
-};
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         // matching all API routes
+//         source: 'https://explorer.chainops.org/api/archway/:path*',
+//         headers: [
+//           { key: 'Access-Control-Allow-Credentials', value: 'true' },
+//           { key: 'Access-Control-Allow-Origin', value: '*' },
+//           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+//           {
+//             key: 'Access-Control-Allow-Headers',
+//             value:
+//               'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
 
 // https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files-experimental
 
