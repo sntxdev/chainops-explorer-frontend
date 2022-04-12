e836c3dd840cd33cf3ab4ab50fea838974413872
Denis Potapov <denipotapo@yandex.ru>
2022-04-12 16:42:36 +0300

    fix cors next.config

--- pages/blocks/index.tsx
+++ pages/blocks/index.tsx
@@ -17,7 +17,7 @@ const Blocks = (props: any) => {
   }, [setAllBlocks]);
 
   const getBlocks = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blocks`, { mode: 'no-cors' });
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/blocks`);
     const data = await res.json();
     setAllBlocks(data);
   };
