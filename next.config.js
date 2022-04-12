b35502669fe16bdf7b943b0ab08e665a4b94f27f
Denis Potapov <denipotapo@yandex.ru>
2022-04-12 16:41:19 +0300

    fix cors next.config

--- next.config.js
+++ next.config.js
@@ -17,6 +17,27 @@ module.exports = {
   },
 };
 
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: 'explorer.chainops.org/api/archway/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+};
+
 // https://nextjs.org/docs/advanced-features/output-file-tracing#automatically-copying-traced-files-experimental
 
 // // Bundle analyzer
